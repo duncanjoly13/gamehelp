ba83f1fe6710dabcda22ee0608e588104605d879
duncanjoly13 <36673745+duncanjoly13@users.noreply.github.com>
2021-02-04 14:45:12 -0600

    Update gamehelp.py

--- gamehelp.py
+++ gamehelp.py
@@ -2,20 +2,20 @@
 # a game assistance module with
 # coin flips, dice, and cards
 # created by Duncan Joly on March 31 April 1 2017
-# Cece is the best
 
 __version__ = 1.0
 
 import random
 
+#flip coin, prints
 def flip():
     sides = ["heads", "tails"]
     choice = random.choice(sides)
-    print(choice)
+    return choice
 
 def roll(sides, numberofDice = 1):
     num = random.randint(numberofDice, sides*numberofDice)
-    print(num)
+    return num
 
 def deal(numOfCards):
     suits = ["hearts", "spades", "diamonds", "clubs"]
@@ -25,9 +25,9 @@ def deal(numOfCards):
     for i in range(cards):
         suit = random.choice(suits)
         number = random.choice(numbers)
-        print(number + " of " + suit)
+        return number + " of " + suit
 
 def spin(n):
 	spun = random.randint(1, n)
-	print(spun)
+	return spun
     
